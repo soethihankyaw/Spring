52ef11f31acac930786bbdbf96417b98b14c6ac8
soegyi <soethiha251@gmail.com>
2024-07-30 23:36:37 +0630

    custom authentication provider

---
+++ spring-with-security/implement-authentication/src/test/java/com/testing/ImplementAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
